511139e93e087eac736892d7f2051dad631e0f65
Daniel Farr <35038598+DanielAptivada@users.noreply.github.com>
2021-03-30 16:32:31 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,18 @@
 <?php 
 include __DIR__ . "/cart.php";
+
+// uncomment the following code, as an example of acceptance criteria
+/*
+$cart = new Cart;
+
+$cart->addProduct(3);
+$cart->addProduct(3); // should have quantity of 2
+
+$cart->addProduct(4, 4);
+$cart->addProduct(4, 5); // should have quantity of 5
+
+$cart->addProduct(1);
+$cart->addProduct(1, 0); // should remove id => 1 from cart
+
+$cart->addProduct(2, 2); // should have quantity 2;
+*/
